9a48f1ebfda8465de5ee8abc8965afd5e6ede075
Artem Andreev <aandreev@aandreev-laptop.local>
2016-05-09 18:10:53 +0300

    messages and main loop

--- roma.asm
+++ roma.asm
@@ -1,58 +1,59 @@
- .model small
- .stack 100h
- .data
- message	db "Hello World!", 0Dh, 0Ah, '$' ; Строка для вывода
+.model small
+.stack 100h
+.data
+m_greeting	db 'Greetings', 0dh, 0ah, '$'
+m_prompt	db 'Enter the number', 0dh, 0ah, '$'
+m_error		db 'Invalid number', 0dh, 0ah, '$'
+m_result	db 'Result ', '$'
+m_quit		db 'Quitting', 0dh, 0ah, '$'
+m_crlf		db 0dh, 0ah, '$'
 
-cr_lf		db 0dh,0ah, '$'
 
-read_buffer label byte
-read_buffer_limit	db 4
-read_buffer_length	db 0
-read_buffer_data 	db 5 dup ('$')
+rbuffer label 
+rbuffer_limit	db 4
+rbuffer_length	db 0
+rbuffer_data 	db 5 dup ('$')
 
  .code                     ; Начало сегмента кода
 
 print proc near
-	push ax
-	push dx
-
 	mov	ax, 0900h
-	mov dx, bx
 	int	21h
-
-	pop dx
-	pop ax
 
 	ret
 print endp
 
-read proc near
-	push ax
-	push dx
-
-	mov ax, 0a00h
-	mov	dx, bx
-	int 21h
-
-	pop	dx
-	pop ax
-
-	ret
-read endp
 
 start:   
 	mov	ax, @data
 	mov	ds, ax
 
-	mov bx, offset read_buffer
-	call read
-
-	mov bx, offset cr_lf
+	mov dx, offset m_greeting
 	call print
 
-	mov bx, offset read_buffer_data
+main:
+	mov dx, offset m_prompt
 	call print
-         
+
+	mov ax, 0a00h
+	mov	dx, offset rbuffer
+	int 21h
+
+	mov dx, offset m_crlf
+	call print
+
+	cmp word ptr [rbuffer_length], 3001h
+	je  exit
+
+	mov dx, offset m_result
+	call print
+
+	mov dx, offset m_crlf
+	call print
+
+	loop main
+
+exit:         
 	mov	ax, 04C00h
 	int	21h
 	
