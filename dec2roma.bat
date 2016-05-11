7fa33ab5424567dcb5756244ce4cf480703e447b
Artem Andreev <aandreev@surfer-172-29-30-1-hotspot.internet-for-guests.com>
2016-05-11 10:38:24 +0200

    TurboDebugger will be run at the end of the build script

--- dec2roma.bat
+++ dec2roma.bat
@@ -3,5 +3,5 @@ set src=C:\SRC
 
 %bin%\TASM.EXE /zi %src%\dec2roma.asm || exit /b
 %bin%\TLINK.EXE /v %src%\dec2roma.obj || exit /b
-rem %bin%\TD.EXE %src%\dec2roma.exe
+%bin%\TD.EXE %src%\dec2roma.exe
 
